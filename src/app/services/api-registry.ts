39157415e52bdd302f4b6ab03bfb91f68096ac24
MBA276F <chau.dang@asiantech.vn>
2019-05-09 16:32:56 +0700

    Add services for struct

---
+++ src/app/services/api-registry.ts
@@ -0,0 +1,14 @@
+export const END_POINT = {
+  authenticate: 'authenticate',
+  users: 'users',
+  home: 'home',
+  tournaments: 'tournaments',
+  matches: 'matches',
+  teams: 'teams',
+  schedules: 'schedules',
+  register: 'register',
+  prediction: 'predictions',
+  tournamentTeams: 'tournamentTeams',
+  statistics: 'statistics',
+  match_detail: 'match-detail',
+}
