a6d6ab18494a16c96bb6134b65e3f35c11c57cc2
chau dang <chau.dang@asiantech.vn>
2019-12-09 09:35:27 +0700

    Test CI

--- src/app/features/features.component.ts
+++ src/app/features/features.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeaturesComponent implements OnInit {
 
+  test: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.test = 123;
   }
 
 }
